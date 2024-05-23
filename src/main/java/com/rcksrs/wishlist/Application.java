55e8c0c09dde0170bab7d13476b45f3dee6428ce
Eryck Soares <rcksrs@outlook.com>
2024-05-22 21:36:07 -0300

    feat: initial commit

---
+++ src/main/java/com/rcksrs/wishlist/Application.java
@@ -0,0 +1,13 @@
+package com.rcksrs.wishlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/rcksrs/wishlist/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcksrs.wishlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
